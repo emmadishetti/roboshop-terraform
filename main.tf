7051a7e2ddc0ce4a766cd8c4b3424a2d57f342e2
suresh <surya2fun@gmail.com>
2023-10-18 23:51:22 +0530

    terraform module roboshop

--- main.tf
+++ main.tf
@@ -102,12 +102,12 @@ module "rabbitmq" {
   env              = var.env
   ami              = var.ami
   zone_id          = var.zone_id
-  ssh_ingress_cidr = var.ssh_ingress_cidr
 
   for_each = var.rabbitmq
 
-  sg_ingress_cidr = local.app_subnets_cidr
-  subnet_ids      = local.db_subnets
-  vpc_id          = local.vpc_id
-  instance_type   = each.value["instance_type"]
+  sg_ingress_cidr  = local.app_subnets_cidr
+  ssh_ingress_cidr = var.ssh_ingress_cidr
+  subnet_ids       = local.db_subnets
+  vpc_id           = local.vpc_id
+  instance_type    = each.value["instance_type"]
 }
