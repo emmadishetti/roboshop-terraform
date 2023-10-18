047ca32867a352b70a68ad02dc4b94fad7b7eb67
suresh <surya2fun@gmail.com>
2023-10-19 00:14:49 +0530

    terraform module roboshop

--- main.tf
+++ main.tf
@@ -96,18 +96,3 @@ module "elasticache" {
 
 }
 
-module "rabbitmq" {
-  source           = "git::https://github.com/emmadishetti/tf-module-rabbitmq.git"
-  tags             = var.tags
-  env              = var.env
-  ami              = var.ami
-  zone_id          = var.zone_id
-
-  for_each = var.rabbitmq
-
-  sg_ingress_cidr  = local.app_subnets_cidr
-  ssh_ingress_cidr = var.ssh_ingress_cidr
-  subnet_ids       = local.db_subnets
-  vpc_id           = local.vpc_id
-  instance_type    = each.value["instance_type"]
-}
